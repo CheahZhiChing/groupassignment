165a7c1bd5c5178856de46faf159b1ed9e61f96c
CheahZhiChing <56948582+CheahZhiChing@users.noreply.github.com>
2019-12-25 20:50:48 +0800

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+
+    Private Sub Table1BindingNavigatorSaveItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Table1BindingNavigatorSaveItem.Click
+        Me.Validate()
+        Me.Table1BindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.Database1DataSet)
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'Database1DataSet.Table1' table. You can move, or remove it, as needed.
+        Me.Table1TableAdapter.Fill(Me.Database1DataSet.Table1)
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        BindingNavigatorDeleteItem.PerformClick()
+        MsgBox("Deleted")
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Table1BindingNavigatorSaveItem.PerformClick()
+        MsgBox("Updated")
+    End Sub
+End Class
