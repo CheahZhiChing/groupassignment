94fd16940392aec1ff585c85ac3683003bf1836a
CheahZhiChing <56948582+CheahZhiChing@users.noreply.github.com>
2019-12-29 14:18:23 +0800

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,319 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Dim YearLabel As System.Windows.Forms.Label
+        Dim Yeild_per_Hectare__Estate___kg_Label As System.Windows.Forms.Label
+        Me.Table1BindingNavigator = New System.Windows.Forms.BindingNavigator(Me.components)
+        Me.BindingNavigatorMoveFirstItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorMovePreviousItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorSeparator = New System.Windows.Forms.ToolStripSeparator()
+        Me.BindingNavigatorPositionItem = New System.Windows.Forms.ToolStripTextBox()
+        Me.BindingNavigatorCountItem = New System.Windows.Forms.ToolStripLabel()
+        Me.BindingNavigatorSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.BindingNavigatorMoveNextItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorMoveLastItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.BindingNavigatorAddNewItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorDeleteItem = New System.Windows.Forms.ToolStripButton()
+        Me.Table1BindingNavigatorSaveItem = New System.Windows.Forms.ToolStripButton()
+        Me.Table1DataGridView = New System.Windows.Forms.DataGridView()
+        Me.YearTextBox = New System.Windows.Forms.TextBox()
+        Me.Yeild_per_Hectare__Estate___kg_TextBox = New System.Windows.Forms.TextBox()
+        Me.Table1BindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.Database1DataSet = New RubberEstates.Database1DataSet()
+        Me.DataGridViewTextBoxColumn1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Table1TableAdapter = New RubberEstates.Database1DataSetTableAdapters.Table1TableAdapter()
+        Me.TableAdapterManager = New RubberEstates.Database1DataSetTableAdapters.TableAdapterManager()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        YearLabel = New System.Windows.Forms.Label()
+        Yeild_per_Hectare__Estate___kg_Label = New System.Windows.Forms.Label()
+        CType(Me.Table1BindingNavigator, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Table1BindingNavigator.SuspendLayout()
+        CType(Me.Table1DataGridView, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Table1BindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Database1DataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Table1BindingNavigator
+        '
+        Me.Table1BindingNavigator.AddNewItem = Me.BindingNavigatorAddNewItem
+        Me.Table1BindingNavigator.BindingSource = Me.Table1BindingSource
+        Me.Table1BindingNavigator.CountItem = Me.BindingNavigatorCountItem
+        Me.Table1BindingNavigator.DeleteItem = Me.BindingNavigatorDeleteItem
+        Me.Table1BindingNavigator.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.BindingNavigatorMoveFirstItem, Me.BindingNavigatorMovePreviousItem, Me.BindingNavigatorSeparator, Me.BindingNavigatorPositionItem, Me.BindingNavigatorCountItem, Me.BindingNavigatorSeparator1, Me.BindingNavigatorMoveNextItem, Me.BindingNavigatorMoveLastItem, Me.BindingNavigatorSeparator2, Me.BindingNavigatorAddNewItem, Me.BindingNavigatorDeleteItem, Me.Table1BindingNavigatorSaveItem})
+        Me.Table1BindingNavigator.Location = New System.Drawing.Point(0, 0)
+        Me.Table1BindingNavigator.MoveFirstItem = Me.BindingNavigatorMoveFirstItem
+        Me.Table1BindingNavigator.MoveLastItem = Me.BindingNavigatorMoveLastItem
+        Me.Table1BindingNavigator.MoveNextItem = Me.BindingNavigatorMoveNextItem
+        Me.Table1BindingNavigator.MovePreviousItem = Me.BindingNavigatorMovePreviousItem
+        Me.Table1BindingNavigator.Name = "Table1BindingNavigator"
+        Me.Table1BindingNavigator.PositionItem = Me.BindingNavigatorPositionItem
+        Me.Table1BindingNavigator.Size = New System.Drawing.Size(786, 27)
+        Me.Table1BindingNavigator.TabIndex = 0
+        Me.Table1BindingNavigator.Text = "BindingNavigator1"
+        '
+        'BindingNavigatorMoveFirstItem
+        '
+        Me.BindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMoveFirstItem.Image = CType(resources.GetObject("BindingNavigatorMoveFirstItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMoveFirstItem.Name = "BindingNavigatorMoveFirstItem"
+        Me.BindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMoveFirstItem.Size = New System.Drawing.Size(23, 24)
+        Me.BindingNavigatorMoveFirstItem.Text = "Move first"
+        '
+        'BindingNavigatorMovePreviousItem
+        '
+        Me.BindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMovePreviousItem.Image = CType(resources.GetObject("BindingNavigatorMovePreviousItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMovePreviousItem.Name = "BindingNavigatorMovePreviousItem"
+        Me.BindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMovePreviousItem.Size = New System.Drawing.Size(23, 24)
+        Me.BindingNavigatorMovePreviousItem.Text = "Move previous"
+        '
+        'BindingNavigatorSeparator
+        '
+        Me.BindingNavigatorSeparator.Name = "BindingNavigatorSeparator"
+        Me.BindingNavigatorSeparator.Size = New System.Drawing.Size(6, 27)
+        '
+        'BindingNavigatorPositionItem
+        '
+        Me.BindingNavigatorPositionItem.AccessibleName = "Position"
+        Me.BindingNavigatorPositionItem.AutoSize = False
+        Me.BindingNavigatorPositionItem.Name = "BindingNavigatorPositionItem"
+        Me.BindingNavigatorPositionItem.Size = New System.Drawing.Size(50, 27)
+        Me.BindingNavigatorPositionItem.Text = "0"
+        Me.BindingNavigatorPositionItem.ToolTipText = "Current position"
+        '
+        'BindingNavigatorCountItem
+        '
+        Me.BindingNavigatorCountItem.Name = "BindingNavigatorCountItem"
+        Me.BindingNavigatorCountItem.Size = New System.Drawing.Size(45, 24)
+        Me.BindingNavigatorCountItem.Text = "of {0}"
+        Me.BindingNavigatorCountItem.ToolTipText = "Total number of items"
+        '
+        'BindingNavigatorSeparator1
+        '
+        Me.BindingNavigatorSeparator1.Name = "BindingNavigatorSeparator"
+        Me.BindingNavigatorSeparator1.Size = New System.Drawing.Size(6, 27)
+        '
+        'BindingNavigatorMoveNextItem
+        '
+        Me.BindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMoveNextItem.Image = CType(resources.GetObject("BindingNavigatorMoveNextItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMoveNextItem.Name = "BindingNavigatorMoveNextItem"
+        Me.BindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMoveNextItem.Size = New System.Drawing.Size(23, 24)
+        Me.BindingNavigatorMoveNextItem.Text = "Move next"
+        '
+        'BindingNavigatorMoveLastItem
+        '
+        Me.BindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMoveLastItem.Image = CType(resources.GetObject("BindingNavigatorMoveLastItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMoveLastItem.Name = "BindingNavigatorMoveLastItem"
+        Me.BindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMoveLastItem.Size = New System.Drawing.Size(23, 24)
+        Me.BindingNavigatorMoveLastItem.Text = "Move last"
+        '
+        'BindingNavigatorSeparator2
+        '
+        Me.BindingNavigatorSeparator2.Name = "BindingNavigatorSeparator"
+        Me.BindingNavigatorSeparator2.Size = New System.Drawing.Size(6, 27)
+        '
+        'BindingNavigatorAddNewItem
+        '
+        Me.BindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorAddNewItem.Image = CType(resources.GetObject("BindingNavigatorAddNewItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorAddNewItem.Name = "BindingNavigatorAddNewItem"
+        Me.BindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorAddNewItem.Size = New System.Drawing.Size(23, 24)
+        Me.BindingNavigatorAddNewItem.Text = "Add new"
+        '
+        'BindingNavigatorDeleteItem
+        '
+        Me.BindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorDeleteItem.Image = CType(resources.GetObject("BindingNavigatorDeleteItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorDeleteItem.Name = "BindingNavigatorDeleteItem"
+        Me.BindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorDeleteItem.Size = New System.Drawing.Size(23, 24)
+        Me.BindingNavigatorDeleteItem.Text = "Delete"
+        '
+        'Table1BindingNavigatorSaveItem
+        '
+        Me.Table1BindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.Table1BindingNavigatorSaveItem.Image = CType(resources.GetObject("Table1BindingNavigatorSaveItem.Image"), System.Drawing.Image)
+        Me.Table1BindingNavigatorSaveItem.Name = "Table1BindingNavigatorSaveItem"
+        Me.Table1BindingNavigatorSaveItem.Size = New System.Drawing.Size(23, 24)
+        Me.Table1BindingNavigatorSaveItem.Text = "Save Data"
+        '
+        'Table1DataGridView
+        '
+        Me.Table1DataGridView.AutoGenerateColumns = False
+        Me.Table1DataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.Table1DataGridView.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.DataGridViewTextBoxColumn1, Me.DataGridViewTextBoxColumn2})
+        Me.Table1DataGridView.DataSource = Me.Table1BindingSource
+        Me.Table1DataGridView.Location = New System.Drawing.Point(31, 170)
+        Me.Table1DataGridView.Name = "Table1DataGridView"
+        Me.Table1DataGridView.RowTemplate.Height = 24
+        Me.Table1DataGridView.Size = New System.Drawing.Size(534, 242)
+        Me.Table1DataGridView.TabIndex = 1
+        '
+        'YearLabel
+        '
+        YearLabel.AutoSize = True
+        YearLabel.Location = New System.Drawing.Point(28, 77)
+        YearLabel.Name = "YearLabel"
+        YearLabel.Size = New System.Drawing.Size(42, 17)
+        YearLabel.TabIndex = 2
+        YearLabel.Text = "Year:"
+        '
+        'YearTextBox
+        '
+        Me.YearTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.Table1BindingSource, "Year", True))
+        Me.YearTextBox.Location = New System.Drawing.Point(239, 74)
+        Me.YearTextBox.Name = "YearTextBox"
+        Me.YearTextBox.Size = New System.Drawing.Size(100, 22)
+        Me.YearTextBox.TabIndex = 3
+        '
+        'Yeild_per_Hectare__Estate___kg_Label
+        '
+        Yeild_per_Hectare__Estate___kg_Label.AutoSize = True
+        Yeild_per_Hectare__Estate___kg_Label.Location = New System.Drawing.Point(28, 105)
+        Yeild_per_Hectare__Estate___kg_Label.Name = "Yeild_per_Hectare__Estate___kg_Label"
+        Yeild_per_Hectare__Estate___kg_Label.Size = New System.Drawing.Size(205, 17)
+        Yeild_per_Hectare__Estate___kg_Label.TabIndex = 4
+        Yeild_per_Hectare__Estate___kg_Label.Text = "Yeild per Hectare (Estate) (kg):"
+        '
+        'Yeild_per_Hectare__Estate___kg_TextBox
+        '
+        Me.Yeild_per_Hectare__Estate___kg_TextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.Table1BindingSource, "Yeild per Hectare (Estate) (kg)", True))
+        Me.Yeild_per_Hectare__Estate___kg_TextBox.Location = New System.Drawing.Point(239, 102)
+        Me.Yeild_per_Hectare__Estate___kg_TextBox.Name = "Yeild_per_Hectare__Estate___kg_TextBox"
+        Me.Yeild_per_Hectare__Estate___kg_TextBox.Size = New System.Drawing.Size(100, 22)
+        Me.Yeild_per_Hectare__Estate___kg_TextBox.TabIndex = 5
+        '
+        'Table1BindingSource
+        '
+        Me.Table1BindingSource.DataMember = "Table1"
+        Me.Table1BindingSource.DataSource = Me.Database1DataSet
+        '
+        'Database1DataSet
+        '
+        Me.Database1DataSet.DataSetName = "Database1DataSet"
+        Me.Database1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'DataGridViewTextBoxColumn1
+        '
+        Me.DataGridViewTextBoxColumn1.DataPropertyName = "Year"
+        Me.DataGridViewTextBoxColumn1.HeaderText = "Year"
+        Me.DataGridViewTextBoxColumn1.Name = "DataGridViewTextBoxColumn1"
+        '
+        'DataGridViewTextBoxColumn2
+        '
+        Me.DataGridViewTextBoxColumn2.DataPropertyName = "Yeild per Hectare (Estate) (kg)"
+        Me.DataGridViewTextBoxColumn2.HeaderText = "Yeild per Hectare (Estate) (kg)"
+        Me.DataGridViewTextBoxColumn2.Name = "DataGridViewTextBoxColumn2"
+        '
+        'Table1TableAdapter
+        '
+        Me.Table1TableAdapter.ClearBeforeFill = True
+        '
+        'TableAdapterManager
+        '
+        Me.TableAdapterManager.BackupDataSetBeforeUpdate = False
+        Me.TableAdapterManager.Table1TableAdapter = Me.Table1TableAdapter
+        Me.TableAdapterManager.UpdateOrder = RubberEstates.Database1DataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(598, 245)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Delete"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(598, 297)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "Save"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(786, 444)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(YearLabel)
+        Me.Controls.Add(Me.YearTextBox)
+        Me.Controls.Add(Yeild_per_Hectare__Estate___kg_Label)
+        Me.Controls.Add(Me.Yeild_per_Hectare__Estate___kg_TextBox)
+        Me.Controls.Add(Me.Table1DataGridView)
+        Me.Controls.Add(Me.Table1BindingNavigator)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.Table1BindingNavigator, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Table1BindingNavigator.ResumeLayout(False)
+        Me.Table1BindingNavigator.PerformLayout()
+        CType(Me.Table1DataGridView, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Table1BindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Database1DataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Database1DataSet As RubberEstates.Database1DataSet
+    Friend WithEvents Table1BindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents Table1TableAdapter As RubberEstates.Database1DataSetTableAdapters.Table1TableAdapter
+    Friend WithEvents TableAdapterManager As RubberEstates.Database1DataSetTableAdapters.TableAdapterManager
+    Friend WithEvents Table1BindingNavigator As System.Windows.Forms.BindingNavigator
+    Friend WithEvents BindingNavigatorAddNewItem As System.Windows.Forms.ToolStripButton
+    Friend WithEvents BindingNavigatorCountItem As System.Windows.Forms.ToolStripLabel
+    Friend WithEvents BindingNavigatorDeleteItem As System.Windows.Forms.ToolStripButton
+    Friend WithEvents BindingNavigatorMoveFirstItem As System.Windows.Forms.ToolStripButton
+    Friend WithEvents BindingNavigatorMovePreviousItem As System.Windows.Forms.ToolStripButton
+    Friend WithEvents BindingNavigatorSeparator As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents BindingNavigatorPositionItem As System.Windows.Forms.ToolStripTextBox
+    Friend WithEvents BindingNavigatorSeparator1 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents BindingNavigatorMoveNextItem As System.Windows.Forms.ToolStripButton
+    Friend WithEvents BindingNavigatorMoveLastItem As System.Windows.Forms.ToolStripButton
+    Friend WithEvents BindingNavigatorSeparator2 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents Table1BindingNavigatorSaveItem As System.Windows.Forms.ToolStripButton
+    Friend WithEvents Table1DataGridView As System.Windows.Forms.DataGridView
+    Friend WithEvents DataGridViewTextBoxColumn1 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn2 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents YearTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents Yeild_per_Hectare__Estate___kg_TextBox As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+
+End Class
